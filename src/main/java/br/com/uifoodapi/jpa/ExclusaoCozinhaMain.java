88e69517025c27e60b70c4bb0fb3c5029e14ca5b
Rafael Madakis <rafaelmadakis@gmail.com>
2022-01-16 21:25:03 -0300

    Excluindo um objeto do banco de dados

---
+++ src/main/java/br/com/uifoodapi/jpa/ExclusaoCozinhaMain.java
@@ -0,0 +1,27 @@
+package br.com.uifoodapi.jpa;
+
+import br.com.uifoodapi.UifoodApiApplication;
+import br.com.uifoodapi.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * @Author: Rafael Madakis
+ */
+public class ExclusaoCozinhaMain {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(UifoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+
+         CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+
+         Cozinha cozinha = new Cozinha();
+         cozinha.setId(1L);
+
+         cadastroCozinha.remover(cozinha);
+    }
+}
